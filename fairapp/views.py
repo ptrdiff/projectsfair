8dcb30896229af1dc7c372e4edce15fbb10eef67
Lysenko Artem <34771157+virrius@users.noreply.github.com>
2018-08-25 17:15:11 +0300

    update search metod. added some features to make it more universal

--- fairapp/views.py
+++ fairapp/views.py
@@ -15,35 +15,49 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.utils import six
+from django.apps import apps
 
 
-def ANDfilter(fieldToFilter,project_list, request):
+def ANDfilter(project_list, request, *fieldToFilter):
     q_dict = dict(six.iterlists(request.GET))
-    print(q_dict)
-    if( not 'skill' in q_dict):
-        return project_list
-    skills = q_dict.get(fieldToFilter)
-    print(skills)
-    etalon=[]
-    for s in skills:
-        etalon.append(Skill.objects.get(pk=s))
-    project_list2 = []
-    print(project_list)
-    for p in project_list:
-        print(etalon)
-        print(list(p.skill.all()))
-        print(set(list(p.skill.all()))&set(etalon)==set(etalon))
-        if set(list(p.skill.all()))&set(etalon)==set(etalon):
+#    print(q_dict)
+
+    for field in fieldToFilter:
+        project_list2 = []
+
+        if(not field in q_dict):
+            continue
+
+        skills = q_dict.get(field)
+#        print(skills)
+        etalon=[]
+        for s in skills:
+            etalon.append(apps.get_model('fairapp', field).objects.get(pk=s))
+
+#        print(project_list)
+        length = len(project_list)
+        for p in range(length):
+            attr=getattr(project_list[p], field)
+
+
+#            print(project_list[p])
+#            print(etalon)
+#            print(list(attr.all()))
+#            print(set(list(attr.all()))&set(etalon)==set(etalon))
+            if set(list(attr.all()))&set(etalon)==set(etalon):
                 #list(p.skill.all()) == etalon:
-            project_list2.append(p)
-    return project_list2
+                project_list2.append(project_list[p])
+
+        project_list=project_list2
+
+    return project_list
 
 
 
 def index(request, page=1):
     object_list = Project.objects.all().exclude(status__in=['m', 'r'])
     project_filter = ProjectFilter(request.GET, queryset=object_list)
-    project_list = ANDfilter('skill', project_filter.qs, request)
+    project_list = ANDfilter(project_filter.qs, request, 'skill', 'tag')
 
     paginator = Paginator(project_list, 5)
     if page > paginator.num_pages:
