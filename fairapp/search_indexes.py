e00304bee8a876739006fe479715cf0192d444bc
nikita03565 <33401112+nikita03565@users.noreply.github.com>
2018-08-28 13:26:50 +0300

    Artem integrated

--- fairapp/search_indexes.py
+++ fairapp/search_indexes.py
@@ -7,6 +7,7 @@ class ProjectIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     project_name = indexes.CharField(model_attr='project_name')
     content = indexes.CharField(model_attr='content')
+    brief_summary = indexes.CharField(model_attr='brief_summary')
 
 
     def get_model(self):
