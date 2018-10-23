cfe244cfa797fbd86789c2379da870e95ba70b67
Salimov Timur <ptrdiff.t@gmail.com>
2018-10-23 18:28:02 +0300

    Deploy travis cicd (#72)
    * Update README.md
    
    * Create Dockerfile
    
    * Create deploy_dockerhub.sh
    
    * Update .travis.yml
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update deploy_dockerhub.sh
    
    * Update travis for heroku

---
+++ .travis/deploy_dockerhub.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+if [ "$TRAVIS_BRANCH" = "deploy-travis-cicd" ]; then
+    TAG="latest"
+else
+    TAG="$TRAVIS_BRANCH"
+fi
+docker build -f Dockerfile -t $DOCKER_USER/$REPO_NAME:$TAG .
+docker push $DOCKER_USER/$REPO_NAME
