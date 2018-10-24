a8094633a6d52638a624bce329b26bd8baea8167
Salimov Timur <ptrdiff.t@gmail.com>
2018-10-24 13:45:16 +0300

    Update deploy_heroku.sh

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
-docker build -f Dockerfile -t registry.heroku.com/$HEROKU_APP_NAME/web .
-docker push registry.heroku.com/$HEROKU_APP_NAME/web
+heroku container:login
+heroku create $HEROKU_APP_NAME
+heroku container:push web -a $HEROKU_APP_NAME
+heroku container:release web -a $HEROKU_APP_NAME
